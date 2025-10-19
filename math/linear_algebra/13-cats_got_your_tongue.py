1a44b351c41b1aab3511942bd0af632bf086be7c
Sara2-3 <sararesulaj40@gmail.com>
2025-10-19 15:11:39 +0200

    Add task 13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for matrix concatenation
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenates two matrices along specific axis
+    Args:
+        mat1: first numpy array
+        mat2: second numpy array
+        axis: axis along which to concatenate (default 0)    
+    Returns:
+        numpy.ndarray: concatenated matrix
+    """
+    return np.concatenate((mat1, mat2), axis=axis)
--- math/linear_algebra/README.md
+++ math/linear_algebra/README.md
@@ -1,1 +1,23 @@
 # Task 0. Slice Me Up
+
+
+# Notes
+# 1. Concatenation axis=0 (vertical - poshtë):
+[[11, 22, 33],
+ [44, 55, 66],
+ [1,  2,  3],
+ [4,  5,  6]]
+Rreshtat: 2 + 2 = 4 rreshta
+Kolonat: Mbeten 3
+
+
+# 2. Concatenation axis=1 (horizontal - djathtas):
+text
+[[11, 22, 33, 1, 2, 3],
+ [44, 55, 66, 4, 5, 6]]
+Rreshtat: Mbeten 2
+Kolonat: 3 + 3 = 6 kolona
+
+axis=0 - bashko poshtë (shto rreshta)
+
+axis=1 - bashko djathtas (shto kolona)
