c7a0a50a09daf6b149717ac4d28a6251f2f55658
Sara2-3 <sararesulaj40@gmail.com>
2025-10-19 16:08:46 +0200

    Add new style

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -8,15 +8,9 @@ def add_arrays(arr1, arr2):
     """
     Adds two arrays element-wise
     """
-    # Kontrollo nëse kanë gjatësi të ndryshme
     if len(arr1) != len(arr2):
         return None
-    
-    # Krijo listën e re
     result = []
-    
-    # Mblidh element pas elementi
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
-    
     return result
