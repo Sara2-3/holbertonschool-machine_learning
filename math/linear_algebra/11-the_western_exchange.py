91e65a2c21116bb1a835a42d306f85402c883cc9
Sara2-3 <sararesulaj40@gmail.com>
2025-10-19 14:51:34 +0200

    Add task 11. The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for writing a function that transpose matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray
+    """
+    return matrix.transpose()
