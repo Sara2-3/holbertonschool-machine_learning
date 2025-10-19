9529985cecc5d6bb6553b489215bfe965382d2c0
Sara2-3 <sararesulaj40@gmail.com>
2025-10-19 15:48:02 +0200

    Changed sth on style

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -8,11 +8,9 @@ import numpy as np
 def np_matmul(mat1, mat2):
     """
     Performs matrix multiplication
-    
     Args:
         mat1: first numpy array
         mat2: second numpy array
-        
     Returns:
         numpy.ndarray: result of matrix multiplication
     """
