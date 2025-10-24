1d2b2c01b3c9e32b53e6656b1e2375b53ca757e1
Sara2-3 <sararesulaj40@gmail.com>
2025-10-24 23:24:37 +0200

    add task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,10 +5,6 @@ Module for calculating the integral of a polynomial.
 
 
 def poly_integral(poly, C=0):
-    """
-    Calculates the integral of a polynomial represented as a list.
-    Returns None if input is invalid.
-    """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
     if not all(isinstance(x, (int, float)) for x in poly):
@@ -23,5 +19,8 @@ def poly_integral(poly, C=0):
         if val == int(val):
             val = int(val)
         integral.append(val)
+
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
 
     return integral
