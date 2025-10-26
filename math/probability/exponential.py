e7293f564edd39cbe64be4043757736c462c60cf
Sara2-3 <sararesulaj40@gmail.com>
2025-10-26 19:01:11 +0100

    Add task 3

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -6,7 +6,7 @@ class Exponential:
     """Represents a Exponential distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        """Initialize Poisson distribution with data or lambtha"""
+        """Initialize Exponential distribution with data or lambtha"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -16,5 +16,6 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            x= (sum(data) / len(data))
-            self.lambtha = float(1/x)
+
+            x = sum(data) / len(data)
+            self.lambtha = float(1 / x)
