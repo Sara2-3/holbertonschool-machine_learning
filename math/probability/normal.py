f8a44c4e91089103ff466e383b98fe4e8d8a74cc
Sara2-3 <sararesulaj40@gmail.com>
2025-10-26 23:00:17 +0100

    Add task 7

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -26,3 +26,11 @@ class Normal:
 
             variance = sum_sq_diff / (len(data) - 1)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return z * self.stddev + self.mean
