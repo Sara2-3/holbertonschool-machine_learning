5924f5eda05d118016f375abb8fa34e84c893673
Sara2-3 <sararesulaj40@gmail.com>
2025-11-07 00:21:06 +0100

    Add task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def bars():
+    """Plot stacked bar chart of fruit quantities per person"""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
