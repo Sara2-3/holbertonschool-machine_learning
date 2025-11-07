c6adbc09f4f8b36f2ed8aec4cd490f8eaf1d78c9
Sara2-3 <sararesulaj40@gmail.com>
2025-11-07 18:34:24 +0100

    Add task 6

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -7,5 +7,6 @@ Module for Flip it and Switch it
 def flip_switch(df):
     """Sorts data in reverse order and transposes it
     """
-    return df.sort_values('Timestamp', ascending=False).T
+    return df.sort_values('Timestamp', ascending=False)
+
 
