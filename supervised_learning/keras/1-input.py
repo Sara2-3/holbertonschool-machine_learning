5bc5e6e4faa98c2001704eba6b01450b02238dc2
Sara2-3 <sararesulaj40@gmail.com>
2026-01-26 14:33:00 +0100

    Add task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     Builds a neural network model using Keras functional API.
 
-    Parameters:
-    nx: number of input features
-    layers: list with number of nodes in each layer
-    activations: list with activation functions used for each layer
-    lambtha: L2 regularization parameter
-    keep_prob: probability that a node will be kept for dropout
+    Parameters
+    ----------
+    nx : int
+        Number of input features to the network.
+    layers : list of int
+        List containing the number of nodes in each layer of the network.
+    activations : list of str
+        List containing the activation functions used for each layer.
+    lambtha : float
+        L2 regularization parameter.
+    keep_prob : float
+        Probability that a node will be kept for dropout.
 
-    Returns:
-    Keras Model
+    Returns
+    -------
+    K.Model
+        The Keras model.
     """
-    # Input layer
     inputs = K.Input(shape=(nx,))
     x = inputs
 
-    # Hidden layers
     for i in range(len(layers)):
-        # Dense layer with L2 regularization
         x = K.layers.Dense(
             units=layers[i],
             activation=activations[i],
             kernel_regularizer=K.regularizers.l2(lambtha)
         )(x)
 
-        # Dropout (not applied on last layer)
         if i < len(layers) - 1:
             x = K.layers.Dropout(rate=1 - keep_prob)(x)
 
-    # Define the model
     model = K.Model(inputs=inputs, outputs=x)
     return model
