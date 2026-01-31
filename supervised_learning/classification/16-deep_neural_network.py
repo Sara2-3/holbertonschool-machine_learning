c639e03b9adf56083ffea8856a2532df2b43e18c
Sara2-3 <sararesulaj40@gmail.com>
2026-01-31 20:44:51 +0100

    Add task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module that defines a deep neural network performing binary classification
+Defines a deep neural network performing binary classification
 """
 
 import numpy as np
@@ -8,7 +8,7 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-    Class that defines a deep neural network performing binary classification
+    Deep neural network class for binary classification
     """
 
     def __init__(self, nx, layers):
@@ -20,7 +20,7 @@ class DeepNeuralNetwork:
         nx : int
             Number of input features
         layers : list
-            List representing the number of nodes in each layer
+            List of number of nodes in each layer
 
         Raises
         ------
@@ -34,21 +34,21 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(nodes, int) and nodes > 0 for nodes in layers):
-            raise TypeError("layers must be a list of positive integers")
+        for nodes in layers:
+            if not isinstance(nodes, int) or nodes < 1:
+                raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for l in range(1, self.L + 1):
-            nodes = layers[l - 1]
-            prev_nodes = nx if l == 1 else layers[l - 2]
-
-            # He initialization for weights
-            self.weights['W' + str(l)] = np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
-            # Biases initialized to zeros
-            self.weights['b' + str(l)] = np.zeros((nodes, 1))
+        for l in range(self.L):
+            layer_key = str(l + 1)
+            nodes = layers[l]
+            prev_nodes = nx if l == 0 else layers[l - 1]
+            self.weights['W' + layer_key] = (
+                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
+            )
+            self.weights['b' + layer_key] = np.zeros((nodes, 1))
