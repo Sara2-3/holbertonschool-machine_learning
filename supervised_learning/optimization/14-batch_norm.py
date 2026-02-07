629888aeee1e7a8ba62870b34086baac1ed9d795
Sara2-3 <sararesulaj40@gmail.com>
2026-02-07 01:25:06 +0100

    Add task 14

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python3
 """
-Module 14-batch_norm
-Provides a function to create a batch normalization layer
-for a neural network in TensorFlow.
+Module 3-mini_batch
+Provides a function to create mini-batches for training a neural network
+using mini-batch gradient descent.
 """
 
-import tensorflow as tf
+import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def create_batch_norm_layer(prev, n, activation):
+def create_mini_batches(X, Y, batch_size):
     """
-    Creates a batch normalization layer for a neural network.
+    Creates mini-batches to be used for training.
 
     Parameters:
-    prev : tensor
-        activated output of the previous layer
-    n : int
-        number of nodes in the layer to be created
-    activation : function
-        activation function to be used on the output
+    X : numpy.ndarray of shape (m, nx)
+    Y : numpy.ndarray of shape (m, ny)
+    batch_size : int
 
     Returns:
-    tensor
-        activated output for the layer
+    list of tuples (X_batch, Y_batch)
     """
-    dense = tf.keras.layers.Dense(
-        units=n,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(
-            mode='fan_avg'
-        ),
-        use_bias=False
-    )(prev)
+    # Shuffle once per epoch
+    X, Y = shuffle_data(X, Y)
+    m = X.shape[0]
+    mini_batches = []
 
-    batch_norm = tf.keras.layers.BatchNormalization(
-        axis=-1,
-        momentum=0.99,
-        epsilon=1e-7,
-        gamma_initializer='ones',
-        beta_initializer='zeros'
-    )(dense)
+    # Slice into batches
+    for i in range(0, m, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+        mini_batches.append((X_batch, Y_batch))
 
-    return activation(batch_norm)
+    return mini_batches
