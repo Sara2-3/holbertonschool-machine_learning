170e868ebc02a2d1d6a7f67011945e0be6e6be54
Sara2-3 <sararesulaj40@gmail.com>
2026-03-02 15:02:52 +0100

    Add task 2

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -9,60 +9,27 @@ import numpy as np
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """
     Performs back propagation over a convolutional layer of a neural network.
-
-    Parameters
-    ----------
-    dZ : numpy.ndarray
-        Shape (m, h_new, w_new, c_new), partial derivatives with respect to
-        the unactivated output of the convolutional layer.
-    A_prev : numpy.ndarray
-        Shape (m, h_prev, w_prev, c_prev), output of the previous layer.
-    W : numpy.ndarray
-        Shape (kh, kw, c_prev, c_new), kernels for the convolution.
-    b : numpy.ndarray
-        Shape (1, 1, 1, c_new), biases applied to the convolution.
-    padding : str, optional
-        Either 'same' or 'valid', type of padding used. Default is 'same'.
-    stride : tuple, optional
-        (sh, sw), strides for the convolution. Default is (1, 1).
-
-    Returns
-    -------
-    dA_prev : numpy.ndarray
-        Partial derivatives with respect to the previous layer.
-    dW : numpy.ndarray
-        Partial derivatives with respect to the kernels.
-    db : numpy.ndarray
-        Partial derivatives with respect to the biases.
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
     _, h_new, w_new, _ = dZ.shape
 
-    # Determine padding
+    # Padding
     if padding == "same":
         ph = ((h_prev - 1) * sh + kh - h_prev) // 2
         pw = ((w_prev - 1) * sw + kw - w_prev) // 2
     else:
         ph, pw = 0, 0
 
-    # Pad A_prev and dA_prev
     A_prev_pad = np.pad(
-        A_prev,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant'
+        A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant'
     )
-    dA_prev_pad = np.pad(
-        np.zeros_like(A_prev),
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant'
-    )
+    dA_prev_pad = np.zeros_like(A_prev_pad)
 
     dW = np.zeros_like(W)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    # Loop over training examples
     for i in range(m):
         a_prev_pad = A_prev_pad[i]
         da_prev_pad = dA_prev_pad[i]
@@ -75,21 +42,20 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    # Slice
                     a_slice = a_prev_pad[vert_start:vert_end,
                                          horiz_start:horiz_end, :]
 
-                    # Update gradients
+                    # Gradient wrt input
                     da_prev_pad[vert_start:vert_end,
                                 horiz_start:horiz_end, :] += (
                         W[:, :, :, c] * dZ[i, h, w, c]
                     )
+
+                    # Gradient wrt weights
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
 
-        # Save back into dA_prev_pad
         dA_prev_pad[i] = da_prev_pad
 
-    # Remove padding
     if padding == "same":
         dA_prev = dA_prev_pad[:, ph:-ph or None, pw:-pw or None, :]
     else:
