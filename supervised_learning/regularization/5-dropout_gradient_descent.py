35fcab5a05a678374fd62fe05288128bbfbe01a8
Sara2-3 <sararesulaj40@gmail.com>
2026-03-15 20:52:57 +0100

    Add task 5

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python3
 """
-Gradient Descent with Dropout
+    Gradient descent with L2 regularization
 """
-
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates the weights of a neural network with Dropout regularization
-    using gradient descent.
+        function that updates weights of NN with Dropout reg
+        using gradient descent
 
-    Parameters:
-    - Y: one-hot numpy.ndarray of shape (classes, m)
-    - weights: dict of weights and biases of the neural network
-    - cache: dict of outputs and dropout masks of each layer
-    - alpha: learning rate
-    - keep_prob: probability that a node will be kept
-    - L: number of layers in the network
-
-    Returns:
-    - None (updates weights in place)
+        :param Y: ndarray, shape(classes,m) correct labels
+        :param weights: dict, weights and biases of NN
+        :param cache: dict, output and dropout mask of each layer
+        :param alpha: learning rate
+        :param keep_prob: proba a node will be kept
+        :param L: number of layer of network
     """
+
+    # store m
     m = Y.shape[1]
-    # derivative for softmax output layer
-    dZ = cache[f"A{L}"] - Y
 
-    for i in range(L, 0, -1):
-        A_prev = cache[f"A{i-1}"]
-        W = weights[f"W{i}"]
+    # derivative of final layer (softmax)
+    A = cache['A' + str(L)]
+    dZ = A - Y
 
-        # gradients
+    # gradient, weight and bias for last layer
+    A_prev = cache['A' + str(L - 1)]
+    W = weights['W' + str(L)]
+    dW = np.matmul(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.matmul(W.T, dZ)
+
+    weights['W' + str(L)] -= alpha * dW
+    weights['b' + str(L)] -= alpha * db
+
+    # with dropout reg
+    for layer in range(L - 1, 0, -1):
+        D = cache['D' + str(layer)]
+        dA = dA_prev * (D / keep_prob)
+
+        A = cache['A' + str(layer)]
+        A_prev = cache['A' + str(layer - 1)]
+        dZ = dA * (1 - A ** 2)
         dW = np.matmul(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
+        W = weights['W' + str(layer)]
+        dA_prev = np.matmul(W.T, dZ)
 
-        # update parameters
-        weights[f"W{i}"] -= alpha * dW
-        weights[f"b{i}"] -= alpha * db
-
-        if i > 1:
-            # propagate error backwards
-            dA_prev = np.matmul(W.T, dZ)
-            # apply dropout mask and scale
-            dA_prev = (dA_prev * cache[f"D{i-1}"]) / keep_prob
-            # derivative of tanh
-            dZ = dA_prev * (1 - cache[f"A{i-1}"] ** 2)
+        weights['W' + str(layer)] -= alpha * dW
+        weights['b' + str(layer)] -= alpha * db
